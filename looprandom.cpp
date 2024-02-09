1e7086297ef54415b59ed0e68fa389fcc5e9f994
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-02-09 10:08:49 -0700

    Create looprandom.cpp

---
+++ looprandom.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<Windows.h>
+using namespace std;
+int main() {
+	bool running = true;
+
+	while (running) {
+		int num = (rand() % 20);
+		for (int i = 0; i < num; i++) {
+			cout << " ";
+		}
+		cout << "<3";
+		Sleep(1);
+	}
+}
